524958415edb2a25b007ff940318225e91f2451f
SophiePelet <peletsophie@gmail.com>
2026-07-11 16:52:46 +0200

    feat: correct the return value of the `getIdea()` function when a wrong index is entered

--- ex01/src/Brain.cpp
+++ ex01/src/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: sophie <sophie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/10 17:08:20 by sophie            #+#    #+#             */
-/*   Updated: 2026/07/10 17:47:37 by sophie           ###   ########.fr       */
+/*   Updated: 2026/07/11 16:48:06 by sophie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,15 +35,20 @@ Brain::~Brain() { std::cout << "Brain destrcutor called\n"; }
 // Member functions
 
 const std::string&	Brain::getIdea(size_t index) const {
+	static const std::string	err_message = "Invalid index\n";
+
 	if (index < 100)
 		return (_ideas[index]);
-	return ("Invalid index\n");
+
+	return (err_message);
 }
 void	Brain::setIdea(size_t index, const std::string content) {
 	if (index < 100) {
 		_ideas[index] = content;
 		return ;
 	}
+
 	std::cout << "Invalid index\n";
+
 	return ;
 }
