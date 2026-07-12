94397454a473c3a9a062e269326501b4b7c68bea
SophiePelet <peletsophie@gmail.com>
2026-07-12 21:31:28 +0200

    refactor: reorganize the main tests in ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sophie <sophie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 16:35:45 by sophie            #+#    #+#             */
-/*   Updated: 2026/07/12 20:56:08 by sophie           ###   ########.fr       */
+/*   Updated: 2026/07/12 21:30:15 by sophie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,18 @@ int main()
 	std::cout << BOLDMAGENTA << "Testing constructor, copy constructor\n" << RESET;
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+
+	i->makeSound();
+	j->makeSound();
+	
+	delete j;
+	delete i;
+
 	Dog dogA;
 	Dog dogB(dogA);
-	Cat catA;
-	Cat catB;
-	catA = catA;
-	Animal* array[5];
-
+	
 	std::cout << BOLDMAGENTA << "Creating and deleting the array of Animal objects\n" << RESET;
+	Animal* array[5];
 	for (size_t i = 0; i < 5; ++i) {
 		if (i < 2)
 			array[i] = new Dog();
@@ -44,15 +48,15 @@ int main()
 		delete array[i];
 	
 	std::cout << BOLDMAGENTA << "Deep copies test\n" << RESET;
+	Cat catA;
+	Cat catB;
+	catA = catA;
 	catA.setIdea(0, "Bite my owner's ankles\n");
 	catB =  catA;
 	catA.setIdea(0, "Meow at the door\n");
 	std::cout << catB.getIdea(0);
 	std::cout << catA.getIdea(0);
 	
-	
-	delete j;
-	delete i;
 
 	return (0);
 }
