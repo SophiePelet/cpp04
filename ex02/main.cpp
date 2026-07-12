9576e30e1188350e7b020acd0d47e40795debb86
SophiePelet <peletsophie@gmail.com>
2026-07-12 21:36:10 +0200

    refactor: reorganize tests in the main of ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sophie <sophie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 16:35:45 by sophie            #+#    #+#             */
-/*   Updated: 2026/07/12 10:46:11 by sophie           ###   ########.fr       */
+/*   Updated: 2026/07/12 21:35:50 by sophie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,47 @@ int main()
 	std::cout << BOLDMAGENTA << "Testing constructor, copy constructor\n" << RESET;
 	const AAnimal* j = new Dog();
 	const AAnimal* i = new Cat();
+	
+	// Triggers copy constructor
 	Dog dogA;
 	Dog dogB(dogA);
-	Cat catA;
-	Cat catB;
-	catA = catA;
+	
+	std::cout << BOLDMAGENTA << "Creating and deleting the array of Animal objects\n" << RESET;
+	AAnimal* array[5];
+	for (size_t i = 0; i < 5; ++i) {
+		if (i < 2)
+			array[i] = new Dog();
+		else
+			array[i] = new Cat();
+	}
+	
+	std::cout << array[1]->getType() << std::endl;
+	array[1]->makeSound();
+	std::cout << array[4]->getType() << std::endl;
+	array[4]->makeSound();
 
-	// Testing AAnimal instantiation
-	// AAnimal	testing;
+	for (size_t i = 0; i < 5; ++i)
+		delete array[i];
+
+
+	//std::cout << BOLDMAGENTA << "Testing AAnimal instantiation\n" << RESET;
+	//const AAnimal* abstract = new AAnimal;
 	
 	std::cout << BOLDMAGENTA << "Testing makeSound() in Cat and Dog\n" << RESET;
 	i->makeSound();
 	j->makeSound();
 	
 	std::cout << BOLDMAGENTA << "Deep copies test\n" << RESET;
+	Cat catA;
+	Cat catB;
 	catA.setIdea(0, "Bite my owner's ankles\n");
-	catB =  catA;
+	catB =  catA; // Triggers assignment operator
 	catA.setIdea(0, "Meow at the door\n");
 	std::cout << catB.getIdea(0);
 	std::cout << catA.getIdea(0);
 	
+	// Self assignment safety check
+	catA = catA;
 	
 	delete j;
 	delete i;
